f6b01b0475e3a5b097742619b730cccc8917c441
BrennanPoker <casillaa4159@masseyhigh.school.nz>
2026-03-25 10:26:32 +1300

    Recipe details - Serving size and Recipe name

---
+++ C_05_Recipe_details.py
@@ -0,0 +1,47 @@
+def num_check(question, num_type="float"):
+    """Checks that response is a float / integer more than zero"""
+
+    # Checks if the number is more than zero
+    if num_type == "float":
+        error = "Please enter a number more than 0."
+    else:
+        error = "Please enter an integer more than 0."
+
+    while True:
+        try:
+
+            if num_type == "float":
+                response = float(input(question))
+            else:
+                response = int(input(question))
+
+            if response > 0:
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
+def not_blank(question):
+    """Checks that a user response is not blank"""
+
+    while True:
+        response = input(question)
+
+        if response != "":
+            return response
+
+        print("Sorry, this can't be blank. Please try again.\n")
+
+
+
+# Main Routine goes here
+
+# Loop for testing purposes
+while True:
+    recipe_name = not_blank("Recipe name: ")
+    serving_size = num_check(question="Serving size: ", num_type="integer")
+    print(f"You are making {serving_size} {recipe_name}")
+    print()
